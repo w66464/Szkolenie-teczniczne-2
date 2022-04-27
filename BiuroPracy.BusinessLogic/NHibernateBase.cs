b50e5bc9bf27c3f77810499a3af0b21ea85f4e55
Dykman <mikolaj.dykman.ext@nokia.com>
2022-04-27 18:54:08 +0200

    Database credentials were added

---
+++ BiuroPracy.BusinessLogic/NHibernateBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace BiuroPracy.BusinessLogic.NHibernate
+{
+    class NHibernateBase
+    {
+        #region Fields
+        private static Configuration configuration { get; set; }
+        private static ISession session { get; set; }
+        private static IStatelessSession statelessSession { get; set; }
+        #endregion
+
+        #region Properties
+        protected static ISessionFactory SessionFactory { get; set; }
+       
+        public static ISession Session => session = SessionFactory.OpenSession();
+        public static IStatelessSession StatelessSession => statelessSession ?? (statelessSession = SessionFactory.OpenStatelessSession());
+        #endregion
+
+        #region Methods
+
+        private static Configuration ConfigureNHibernate()
+        {
+            try
+            {
+                configuration = new Configuration();
+                configuration.Configure();
+                return configuration;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+         
+        }
+
+        public void Initialize()
+        {
+            configuration = ConfigureNHibernate();
+            SessionFactory = configuration.BuildSessionFactory();
+            new SchemaExport(configuration);
+        }
+
+        #endregion
+    }
+}
